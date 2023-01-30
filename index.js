20f04997f8d8c52a8cbb783b8c8b9660ad2797c6
unknown <mayedakonika@gmail.com>
2023-01-30 13:46:25 +0600

    fakedata and projects api added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const projects = require("./fakedata/projects.json");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("api loading");
+});
+app.get("/projects", async (req, res) => {
+  res.send(projects);
 });
 app.listen(port, () => {
   console.log("personal portfolio", port);
